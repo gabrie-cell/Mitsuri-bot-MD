2030c8acd0b0fc1fe59a0eec0c260a1089075de1
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-02-07 16:04:15 -0500

    Crear juegos-top.js

---
+++ plugins/juegos-top.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn }) => {
+  let users = Object.entries(db.data.users).filter(([_, user]) => user.puntos > 0 && user.clashroyale)
+  users = users.sort((a, b) => b[1].puntos - a[1].puntos)
+  console.log(users)
+  let texto = '*Top 10 de mayores ganadas en Clash Royale:*\n\n'
+  for (let i = 0; i < 10 && i < users.length; i++) {
+    let [nombre, user] = users[i]
+    texto += `${i + 1}. ${nombre} - ${user.puntos} puntos\n`
+  }
+  m.reply(texto, null, { mentions: users.map(([nombre]) => nombre) })
+}
+
+handler.help = ['topclash']
+handler.tags = ['juegos', 'clashroyale']
+handler.command = ['topclash']
+
+export default handler
